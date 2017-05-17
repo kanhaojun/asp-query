45d7b1ba3c069979c1fe14bb9eebf6bcb87039bb
kanhaojun <kanhaojun.ascdfkan@gmail.com>
2017-05-18 02:49:02 +0800

    Upload project HW

---
+++ Default.aspx
@@ -0,0 +1,48 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>Jun</title>
+	<script type = "text/javascript" src = "js/main.js"></script>
+    <link rel = "stylesheet" type = "text/css" href = "css/main.css" />
+    <link rel = "stylesheet" href = "css/bootstrap.min.css" />
+    <link rel = "stylesheet" href = "css/bootstrap.css" />
+    <script src="js/bootstrap.min.js"></script>
+    <link rel="shortcut icon" href="favicon.ico"/>
+</head>
+<body class = "bg-color">
+    <form id="form1" runat="server">
+    <div style ="text-align:center;">
+        <br />
+        <div>
+            <h1>Query Products</h1>
+        </div>
+        <br />
+        <hr />
+        <div>
+            <h2>檢視全部資料筆數</h2>
+        <br /> 
+          <asp:Label ID="showdb" runat="server"></asp:Label>
+        <br />
+        </div>
+        <br />
+        <hr />
+        <div>
+            <h2>查詢</h2>
+        <br /> 
+            <asp:TextBox ID = "querytbx"  CssClass = "btn-text btn-wdt30" runat="server" ></asp:TextBox>
+        <br />
+            <asp:Button ID = "querybtn" CssClass = "btn btn-primary btn-wdt30" runat="server" Text="Query" OnClick="imds_Click1" />
+        <br />
+        <br />
+            <asp:Label ID="qrylab" runat="server"></asp:Label>
+        </div>
+
+
+    </div>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+public partial class _Default : System.Web.UI.Page
+{
+    SqlConnection objCon, objCon2;
+    SqlCommand objCmd, objCmd2;
+    SqlDataReader objDR, objDR2;
+    string strDbCon, strDbCon2, strallSQL, srtqrySQL;
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        /* 資料庫連接字串 */
+        strDbCon = "Data Source=(LocalDB)\\MSSQLLocalDB;" +
+                   "AttachDbFilename=" +
+                   Server.MapPath("App_Data\\Product.mdf") +
+                   ";Integrated Security=True";
+
+        /* 建立Connection物件 */
+        objCon = new SqlConnection(strDbCon);
+        /* 開始資料庫連接  */
+        objCon.Open();
+        /*建立Command物件的SQL指令 */
+        strallSQL = "SELECT * FROM Product";
+        /*取得DataReader物件 */
+        objCmd = new SqlCommand(strallSQL, objCon);
+
+        objDR = objCmd.ExecuteReader();
+        if (objDR.HasRows)
+        {
+            /* 顯示資料表的記錄 */
+            showdb.Text = " ";
+
+            while (objDR.Read())
+            {
+                showdb.Text += objDR["number"] + " - ";
+                showdb.Text += objDR["price"] + "<br />";
+            }
+        }
+        else
+        {
+            showdb.Text = "資料表中沒有記錄資料!";
+        }
+        objDR.Close(); // 關閉DataReader
+        objCon.Close(); // 關閉資料庫連接
+    }
+
+    protected void imds_Click1(object sender, EventArgs e)
+    {
+        string temQrytn;
+        
+        temQrytn = querytbx.Text;
+        
+        /* 資料庫連接字串 */
+        strDbCon2 = "Data Source=(LocalDB)\\MSSQLLocalDB;" +
+                   "AttachDbFilename=" +
+                   Server.MapPath("App_Data\\Product.mdf") +
+                   ";Integrated Security=True";
+
+        /* 建立Connection物件 */
+        objCon2 = new SqlConnection(strDbCon);
+        /* 開始資料庫連接  */
+        objCon2.Open();
+        /*建立Command物件的SQL指令 */
+        srtqrySQL = "SELECT * FROM Product WHERE number ='" + temQrytn + "'";
+        /*取得DataReader物件 */
+        objCmd2 = new SqlCommand(srtqrySQL, objCon2);
+
+        objDR2 = objCmd2.ExecuteReader();
+        if (objDR2.HasRows)
+        {
+            /* 顯示資料表的記錄 */
+           qrylab .Text = " ";
+
+            while (objDR2.Read())
+            {
+              qrylab.Text += objDR2["number"] + " - ";
+              qrylab.Text += objDR2["price"] + "<br />";
+           
+            }
+        }
+        else
+        {
+                qrylab.Text = "資料表中沒有記錄資料!";
+        }
+        objDR2.Close(); // 關閉DataReader
+        objCon2.Close(); // 關閉資料庫連接
+    }
+}
